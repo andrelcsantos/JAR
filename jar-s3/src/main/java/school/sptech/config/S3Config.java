6e291ba24d9e53d80b35ab0b537607b336ee1ac0
andrelcsantos <andre.csantos@sptech.school>
2026-04-24 18:17:04 -0300

    feat:Subindo uma parte do JAR da S3

---
+++ jar-s3/src/main/java/school/sptech/config/S3Config.java
@@ -0,0 +1,14 @@
+package school.sptech.config;
+
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class S3Config {
+    public static S3Client getS3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .build();
+    }
+}
---
+++ jar-s3/src/main/java/school/sptech/service/S3Service.java
@@ -0,0 +1,19 @@
+package school.sptech.service;
+
+import school.sptech.config.S3Config;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import java.io.InputStream;
+
+public class S3Service {
+    private final S3Client s3 = S3Config.getS3Client();
+    private final String bucketNome = "bucket-leitura-java-039048919469-us-east-1-an";
+
+    public InputStream obterArquivo(String chaveS3) {
+        GetObjectRequest request = GetObjectRequest.builder()
+                .bucket(bucketNome)
+                .key(chaveS3)
+                .build();
+        return s3.getObject(request);
+    }
+}
